2bf9ab25fb3d55df5779f8e475b315a6a63afa8f
tiostwn <satriyo.stwn@gmail.com>
2023-11-27 11:49:35 +0700

    add: files

---
+++ app/Views/pages/contact.php
@@ -0,0 +1,7 @@
+<div class="grid h-screen place-items-center bg-white-100">
+    <div class="flex flex-col justify-center items-center pb-24">
+        <h4 class="text-5xl font-reguler text-slate-900">You are in <span class="text-sky-600 font-bold">Contact</span> view</h4>
+        
+    </div>    
+</div>
+
